fd2ac2a35e0f9106991531fa5f2ae98b422e4335
dethMastery <owner@detzz.in.th>
2022-08-11 22:41:20 +0700

    finished: code

---
+++ main.js
@@ -0,0 +1,43 @@
+let rpc = require('discord-rpc')
+let cli = new rpc.Client({transport: 'ipc'})
+
+let cliID = '942719157055655956'
+
+rpc.register(cliID)
+
+async function setActivity() {
+  if (!cli) return;
+
+  cli.setActivity({
+    details: `Pastel Delightful`,
+    state: `In process of making new album`,
+    startTimestamp: Date.now(),
+    largeImageKey: `katchan`,
+    largeImageText: `hiya this is katchan`,
+    smallImageKey: `none`,
+    smallImageText: `nothing here`,
+
+    buttons: [
+      {
+        label: `HomePage`,
+        url: `https://suphakit.net`
+      },
+      {
+        label: `Donation`,
+        url: `https://suphakit.net/Donation`
+      }
+    ],
+  
+    instance: true
+  })
+}
+
+cli.on('ready', async () => {
+  setActivity()
+
+  setInterval(() => {
+    setActivity()
+  }, 15 * 1000)
+})
+
+cli.login({ clientId: cliID}).catch(err => console.error(err))
